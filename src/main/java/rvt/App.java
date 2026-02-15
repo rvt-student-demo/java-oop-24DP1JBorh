fe7567a321e33cd1047574c01b48b9fe80458b8d
24DP1JBorh <a242182jb@rvt.lv>
2026-02-15 13:35:28 +0200

    TodoList 2 parts

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,16 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+
+        // Here you can try out the combined functionality of your classes
+        TodoList2Parts list = new TodoList2Parts();
+        Scanner scanner = new Scanner(System.in);
+
+        UserInterface ui = new UserInterface(list, scanner);
+
+        ui.start();
     }
 }
