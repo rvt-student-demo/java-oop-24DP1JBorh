b174f9b87f8d07fdd03124e177542a6f558c9e90
24DP1JBorh <a242182jb@rvt.lv>
2026-03-18 11:15:28 +0200

    Interface in a box (unfinished)

---
+++ src/main/java/Interface_in_a_box/Box.java
@@ -0,0 +1,9 @@
+package Interface_in_a_box;
+
+public class Box {
+    private ArrayList<Packable> box;
+
+    public Box() {
+        this.box = new ArrayList<>();
+    }
+}
---
+++ src/main/java/Interface_in_a_box/Packable.java
@@ -0,0 +1,5 @@
+package Interface_in_a_box;
+
+public interface Packable {
+    double weight();
+}
